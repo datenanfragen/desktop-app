b0facb6d7b56e638e2ed5a53cf093e5822c8318f
Lorenz Sieben <me@lorenz-sieben.com>
2022-08-26 19:57:14 +0200

    WIP: Imap import

--- src/app/js/context-bridge.d.ts
+++ src/app/js/context-bridge.d.ts
@@ -3,6 +3,8 @@ import type {
     SendMessageReturn,
     RecreateEmailClientsOptions,
     RecreateEmailClientsReturn,
+    GetMessageOptions,
+    GetMessageResult,
 } from '../../electron/email';
 
 declare global {
@@ -12,6 +14,8 @@ declare global {
             verifyConnection: () => Promise<boolean>;
             setEmailAccountPassword: (protocol: 'imap' | 'smtp', password: string) => Promise<void>;
             sendMessage: (options: SendMessageOptions) => SendMessageReturn;
+            getFolders: () => Promise<string[]>;
+            getMessages: (options: GetMessageOptions) => Promise<GetMessageResult[]>;
         };
     }
 }
