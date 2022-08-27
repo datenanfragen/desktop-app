a55ba382f490e1fab67935961554423d981c1770
Benjamin Altpeter <benjamin@oldpeter.de>
2022-08-27 20:50:03 +0200

    Optional offline cache of the company database

--- src/app/js/settings.tsx
+++ src/app/js/settings.tsx
@@ -1,13 +1,25 @@
-import { EmailAccountSettingsInput, flash, FlashMessage, I18nWidget } from '@datenanfragen/components';
+import {
+    EmailAccountSettingsInput,
+    flash,
+    FlashMessage,
+    I18nWidget,
+    useCacheStore,
+    useAppStore,
+} from '@datenanfragen/components';
+import hardcodedOfflineData from '@datenanfragen/components/dist/offline-data.json';
 import { IntlProvider, Text } from 'preact-i18n';
 import { useAppSettingsStore } from './store/settings';
 
 export const Settings = () => {
-    const [setEmailAccountSetting, setReceiveNotifications] = useAppSettingsStore((state) => [
+    const [setEmailAccountSetting, setReceiveNotifications, setUseOfflineSearch] = useAppSettingsStore((state) => [
         state.setEmailAccountSetting,
         state.setReceiveNotifications,
+        state.setUseOfflineSearch,
     ]);
-    const receiveNotifications = useAppSettingsStore((state) => state.receiveNotifications);
+    const [receiveNotifications, useOfflineSearch] = useAppSettingsStore((state) => [
+        state.receiveNotifications,
+        state.useOfflineSearch,
+    ]);
     const emailAccountSettings = useAppSettingsStore((state) => ({
         imapUser: state.imapUser,
         imapHost: state.imapHost,
@@ -22,6 +34,11 @@ export const Settings = () => {
         smtpUseStartTls: true,
     }));
 
+    const savedLocale = useAppStore((state) => state.savedLocale);
+
+    const [offlineData, updateOfflineData] = useCacheStore((state) => [state.offlineData, state.updateOfflineData]);
+    const offlineDataDate = offlineData ? JSON.parse(offlineData).date : hardcodedOfflineData.date;
+
     return (
         <IntlProvider definition={window.I18N_DEFINITION_APP} scope="settings">
             <h1>
@@ -29,24 +46,61 @@ export const Settings = () => {
             </h1>
 
             <div>
-                <I18nWidget
-                    minimal={true}
-                    showLanguageOnly={false}
-                    saveLanguagesToStore={true}
-                    onSavedLanguage={() => window.location.reload()}
-                />
+                <fieldset style="margin-bottom: 20px;">
+                    <legend>
+                        <Text id="i18n" />
+                    </legend>
 
-                <hr style="margin: 15px 0;" />
-
-                <label>
-                    <input
-                        checked={receiveNotifications}
-                        type="checkbox"
-                        className="form-element"
-                        onChange={(e) => setReceiveNotifications(e.currentTarget.checked)}
+                    <I18nWidget
+                        minimal={true}
+                        showLanguageOnly={false}
+                        saveLanguagesToStore={true}
+                        onSavedLanguage={() => window.location.reload()}
                     />
-                    <Text id="receive-notifications" />
-                </label>
+                </fieldset>
+
+                <fieldset style="margin-bottom: 20px;">
+                    <legend>
+                        <Text id="features" />
+                    </legend>
+
+                    <label>
+                        <input
+                            checked={receiveNotifications}
+                            type="checkbox"
+                            className="form-element"
+                            onChange={(e) => setReceiveNotifications(e.currentTarget.checked)}
+                        />
+                        <Text id="receive-notifications" />
+                    </label>
+                    <br />
+                    <label>
+                        <input
+                            checked={useOfflineSearch}
+                            type="checkbox"
+                            className="form-element"
+                            onChange={(e) => setUseOfflineSearch(e.currentTarget.checked)}
+                        />
+                        <Text id="use-offline-search" />
+                    </label>
+                    {useOfflineSearch && (
+                        <>
+                            <br />
+                            <Text
+                                id="offline-search-updated-at"
+                                fields={{
+                                    date: new Date(offlineDataDate).toLocaleString(savedLocale, {
+                                        dateStyle: 'long',
+                                        timeStyle: 'medium',
+                                    }),
+                                }}
+                            />{' '}
+                            <button className="button button-secondary button-small" onClick={updateOfflineData}>
+                                <Text id="offline-search-update" />
+                            </button>
+                        </>
+                    )}
+                </fieldset>
 
                 <EmailAccountSettingsInput
                     emailAccountSettings={emailAccountSettings}
